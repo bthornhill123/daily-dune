753012b58f9eb8ead999a2b7314bb1c0d6f6ce63
Ben Thornhill <25141193+bthornhill123@users.noreply.github.com>
2025-07-13 13:05:51 -0600

    Update firebase.config.ts

--- frontend/config/firebase.config.ts
+++ frontend/config/firebase.config.ts
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC-Az5URGOdwCGYMFH-xq7hN6cKc84JBnk",
-    authDomain: "daily-dune.firebaseapp.com",
-    projectId: "daily-dune",
-    storageBucket: "daily-dune.appspot.com",
-    messagingSenderId: "12444061561",
-    appId: "1:12444061561:web:be3505f905e2707b02a9a8",
-    measurementId: "G-1SB15ZZKEF"
-};
+export const firebaseConfig = {};
